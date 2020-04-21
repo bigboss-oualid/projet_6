96002afa988b350b57e49b20364ec5dc041f21c4
oualid boulatar <oualid.boulatar@it-bigboss.de>
2020-04-21 02:46:00 +0200

    feat: show single-page trick

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,6 +24,30 @@ class BlogController extends AbstractController
 		return $this->render('pages/home.html.twig', [
 			'current_menu'    => 'home',
 			'tricks' => $tricks
+		]);
+	}
+
+	/**
+	 * @Route("/tricks/{slug}/{id}", name="pages.show", requirements={"slug": "[a-z0-9\-]*", "id": "\d+"})
+	 * @param Trick   $trick
+	 * @param String  $slug
+	 * @param Request $request
+	 *
+	 * @return Response
+	 */
+	public function show(Trick $trick, String $slug,  Request $request)
+	{
+		if ($trick->getSlug() != $slug) {
+			return $this->redirectToRoute('pages.show', [
+				'slug' => $trick->getSlug(),
+				'id'   => $trick->getId()
+			], 301);
+		}
+
+		return $this->render('pages/show.html.twig', [
+			'controller_name' => 'BlogController',
+			'current_menu'    => 'show',
+			'trick'           => $trick
 		]);
 	}
 }
