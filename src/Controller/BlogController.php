d80d5a6f663f814887766b9551f50b357fd6f5ed
oualid boulatar <oualid.boulatar@it-bigboss.de>
2020-05-20 20:18:31 +0200

    feat: add load more button to get more tricks or comments

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,9 +8,12 @@ use App\Entity\Trick;
 use App\Entity\User;
 use App\Form\CommentType;
 use App\Form\RatingType;
-use App\Repository\TrickRepository;
+use App\Service\Pagination;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,24 +22,88 @@ class BlogController extends AbstractController
 {
 	/**
 	 * @Route("/", name="blog.home")
-	 * @param TrickRepository $repository
 	 *
-	 * @return Response
+	 * @param Pagination $pagination
+	 * @param Request    $request
+	 *
+	 * @return Response | JsonResponse
+	 * @throws \Twig\Error\LoaderError
+	 * @throws \Twig\Error\RuntimeError
+	 * @throws \Twig\Error\SyntaxError
 	 */
-	public function home(TrickRepository $repository): Response
+	public function home(Pagination $pagination, Request $request)
 	{
-		if($this->getUser()){
-			$tricks = $repository->findAll();
-		}else{
-			$tricks = $repository->findBy([
-				"published" => true
-			]);
+		$pagination->setEntityClass(Trick::class);
+		if($request->isXmlHttpRequest()){
+			$pagination->setEntityTemplatePath('blog/include/_pagination_tricks.html.twig');
+
+			return new JsonResponse($pagination->renderView(), 200, [], true);
 		}
 
+		$pagination->setRoute('/');
+
 		return $this->render('blog/home.html.twig', [
-			'current_menu'    => 'home',
-			'tricks' => $tricks
+			'current_menu'  => 'home',
+			'pagination' => $pagination,
 		]);
+	}
+
+
+	/*public function loadTricks(Pagination $pagination, Request $request): JsonResponse{
+
+		if($request->isXmlHttpRequest()){
+			$pagination->setEntityClass(Trick::class)
+				->setEntityTemplatePath('blog/include/_pagination_tricks.html.twig');
+
+			return new JsonResponse($pagination->renderView(), 200, [], true);
+		}
+
+		return new JsonResponse([
+			'type' => 'error',
+			'message' => 'request is not with ajax'
+		]);
+	}*/
+
+	/**
+	 * @Route("/trick/{id}/add-comment", name="add_comment", requirements={"id": "\d+"})
+	 * @IsGranted("ROLE_USER")
+	 *
+	 * @param Trick                  $trick
+	 * @param EntityManagerInterface $em
+	 * @param Request                $request
+	 *
+	 * @return JsonResponse
+	 */
+	public function addComment(Trick $trick, EntityManagerInterface $em, Request $request): JsonResponse{
+		$user = $this->getUser();
+
+		if($request->isXmlHttpRequest()) {
+			$comment = new Comment();
+			$form = $this->createForm(CommentType::class, $comment);
+			$form->handleRequest($request);
+
+			if(!$form->isValid()) {
+				$errors = $this->getErrorsFromForm($form);
+				$data = [
+					'type' => 'validation_error',
+					'title' => 'There was a validation error',
+					'errors' => $errors
+				];
+				return new JsonResponse($data, 400);
+			}
+
+			$comment->setTrick($trick)->setAuthor($user);
+			$em->persist($comment);
+			$em->flush();
+			$html = $this->renderView('blog/include/_pagination_comment.html.twig', ['comments' => [$comment]]);
+
+			return new JsonResponse(['comment' => $html], 200);
+		}
+
+		return new JsonResponse([
+			'type' => 'error',
+			'title' => 'Request is not isXmlHttpRequest'
+		], 500);
 	}
 
 	/**
@@ -45,10 +112,14 @@ class BlogController extends AbstractController
 	 * @param String                 $slug
 	 * @param Request                $request
 	 * @param EntityManagerInterface $em
+	 * @param Pagination             $pagination
 	 *
 	 * @return Response
+	 * @throws \Twig\Error\LoaderError
+	 * @throws \Twig\Error\RuntimeError
+	 * @throws \Twig\Error\SyntaxError
 	 */
-	public function show(Trick $trick, String $slug, Request $request, EntityManagerInterface $em): Response
+	public function show(Trick $trick, String $slug, Request $request, EntityManagerInterface $em, Pagination $pagination): Response
 	{
 		if ($trick->getSlug() != $slug) {
 			return $this->redirectToRoute('blog.show', [
@@ -57,51 +128,72 @@ class BlogController extends AbstractController
 			], 301);
 		}
 
+		$pagination->setEntityClass(Comment::class)->setCriteria(['trick' =>$trick]);
+		if($request->isXmlHttpRequest()){
+			$pagination->setEntityTemplatePath('blog/include/_pagination_comment.html.twig');
+
+			return new JsonResponse($pagination->renderView(), 200, [], true);
+		}
+
+		$pagination->setRoute('/tricks/' . $slug . '/'. $trick->getId());
+
 		/** @var User $user */
-		$user = $this->getUser();
+		if($user = $this->getUser()){
 
-		$comment = new Comment();
+			$comment = new Comment();
 
-		$rating = $em->getRepository(Rating::class)->findOneBy(['trick' =>$trick, 'author'=>$user]);
-		if(!$rating){
-			$rating = new Rating();
+			$rating = $em->getRepository(Rating::class)->findOneBy(['trick' =>$trick, 'author'=>$user]);
+			if(!$rating){
+				$rating = new Rating();
+			}
+
+			$commentForm = $this->createForm(CommentType::class, $comment);
+
+			$ratingForm = $this->createForm(RatingType::class, $rating);
+			$ratingForm->handleRequest($request);
+
+			if ($ratingForm->isSubmitted() && $ratingForm->isValid()) {
+				$rating->setTrick($trick)
+					->setAuthor($user);
+				$em->persist($rating);
+				$em->flush();
+				$this->addFlash('success', "Votre èvaluation de diffuculté a bien é´te pris en compte!");
+			}
+
+			return $this->render('blog/show.html.twig', [
+				'current_menu'    => 'show',
+				'trick'           => $trick,
+				'rating_form'     => $ratingForm->createView(),
+				'comment_form'    => $commentForm->createView(),
+				'pagination' => $pagination
+			]);
 		}
-
-		$commentForm = $this->submitForm(CommentType::class, $trick, $user, $comment, "Votre commentaire a bien été pris en compte !", $em, $request);
-
-		$ratingForm = $this->submitForm(RatingType::class, $trick, $user, $rating, "Votre èvaluation de diffuculté a bien é´te pris en compte!", $em, $request);
-
-		if($commentForm->getData()->getId()){
-			return $this->redirectToRoute('blog.show', [
-				'slug' => $trick->getSlug(),
-				'id'   => $trick->getId(),
-				'_fragment' => 'comment'
-			], 301);
-		}
-
 		return $this->render('blog/show.html.twig', [
 			'current_menu'    => 'show',
 			'trick'           => $trick,
-			'rating_form'     => $ratingForm->createView(),
-			'comment_form'    => $commentForm->createView()
+			'pagination' => $pagination,
 		]);
 	}
 
-	private function submitForm(String $formType, Trick $trick,User $user, $persist, String $successMessage, EntityManagerInterface $em, Request $request){
-
-		$form = $this->createForm($formType, $persist);
-
-		if($request->isMethod('POST')){
-			$form->handleRequest($request);
-			if ($form->isSubmitted() && $form->isValid()) {
-				$persist->setTrick($trick)
-					->setAuthor($user);
-				$em->persist($persist);
-				$em->flush();
-				$this->addFlash('success', $successMessage );
+	/**
+	 * use errors in ajax request
+	 * @param FormInterface $form
+	 *
+	 * @return array
+	 */
+	private function getErrorsFromForm(FormInterface $form)
+	{
+		$errors = array();
+		foreach ($form->getErrors() as $error) {
+			$errors[] = $error->getMessage();
+		}
+		foreach ($form->all() as $childForm) {
+			if ($childForm instanceof FormInterface) {
+				if ($childErrors = $this->getErrorsFromForm($childForm)) {
+					$errors[$childForm->getName()] = $childErrors;
+				}
 			}
 		}
-
-		return $form;
+		return $errors;
 	}
 }
